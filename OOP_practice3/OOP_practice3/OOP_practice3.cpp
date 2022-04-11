c2c17e68ac196863736d7144ba5c0a6405732d29
Walter <82023802+WalSnake@users.noreply.github.com>
2022-04-11 21:09:36 +0500

    OOP_practice3-0.2
    Task 2 completed... I think

--- OOP_practice3/OOP_practice3/OOP_practice3.cpp
+++ OOP_practice3/OOP_practice3/OOP_practice3.cpp
@@ -42,7 +42,65 @@ class circle : public figure {
     };
 };
 
+//Task #2
+class Car {
+protected:
+    std::string company;
+    std::string model;
+public:
+    Car() {}
+    Car(std::string company, std::string model) {
+        this->company = company;
+        this->model = model;
+        std::cout << "Car: " << company << " " << model << std::endl;
+    }
+};
+
+class PassengerCar : public Car {
+public:
+    PassengerCar() {}
+    PassengerCar(std::string company, std::string model) {
+        this->company = company;
+        this->model = model;
+        std::cout << "Passenger Car: " << company << " " << model << std::endl;
+    }
+};
+
+class Bus : public Car {
+protected:
+    std::string company;
+    std::string model;
+public:
+    Bus() {}
+    Bus(std::string company, std::string model) {
+        this->company = company;
+        this->model = model;
+        std::cout << "Bus: " << company << " " << model << std::endl;
+    }
+};
+
+class Minivan : public PassengerCar, public Bus {
+protected:
+    std::string company;
+    std::string model;
+public:
+    Minivan() {}
+    Minivan(std::string company, std::string model) {
+        this->company = company;
+        this->model = model;
+        std::cout << "Minivan: " << company << " " << model << std::endl;
+    }
+};
+
 int main()
 {
-    
+    Car c1("Ford", "Model T");
+    Car c2("Wolksvagen", "Bitle");
+    PassengerCar pc1("Toyota", "Camry");
+    PassengerCar pc2("Peugeot", "406");
+    Bus b1("Scania", "some model");
+    Bus b2("Mercedes", "whatever model");
+    Minivan m1("Mitsubishi", "Delica");
+    Minivan m2("Honda", "Odyssey");
+    //I didn't understood, what is "diamond of death"...
 }
